c12ca6f566be2e3f48bd84bf6d6f25dcace5eb7c
Dave Young <solenovex@live.com>
2017-11-01 19:51:06 +0800

    update

--- SharedSettings/Settings/CoreApiSettings.cs
+++ SharedSettings/Settings/CoreApiSettings.cs
@@ -13,7 +13,7 @@ namespace SharedSettings.Settings
         public static string CorsOrigin = "http://localhost:4200";
         public static (string Name, string DisplayName) CoreApiResource = ("coreapi", "Core APIs");
         public static (string ClientId, string ClientName, string RedirectUris, string PostLogoutRedirectUris, string AllowedCorsOrigins) Client =
-            ("corejs", "Core Javascript Client", "http://localhost:4200/callback.html", "http://localhost:4200/index.html", "http://localhost:4200");
+            ("corejs", "Core Javascript Client", "http://localhost:4200/login-callback", "http://localhost:4200/index.html", "http://localhost:4200");
 
         #endregion
     }
