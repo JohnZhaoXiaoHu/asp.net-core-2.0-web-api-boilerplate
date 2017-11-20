907dd74a4d197f9a663faf553cef4a8b4033268a
Dave Young <solenovex@live.com>
2017-11-20 13:15:25 +0800

    update

---
+++ SalesApi.Web/Controllers/Core/EnumController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SalesApi.Web.Controllers.Bases;
+using SharedSettings.Enums;
+
+namespace SalesApi.Web.Controllers.Core
+{
+    [Route("api/sales/[controller]")]
+    public class EnumController : SalesController<EnumController>
+    {
+        public EnumController(ICoreService<EnumController> coreService) : base(coreService)
+        {
+
+        }
+
+        [HttpGet]
+        [Route("SalesType")]
+        [AllowAnonymous]
+        public IActionResult GetSalesType()
+        {
+            var types = Enum.GetValues(typeof(SalesType)).OfType<SalesType>().Select(x => new KeyValuePair<string, SalesType>(x.ToString(), x)).ToList();
+            return Ok(types);
+        }
+    }
+}
