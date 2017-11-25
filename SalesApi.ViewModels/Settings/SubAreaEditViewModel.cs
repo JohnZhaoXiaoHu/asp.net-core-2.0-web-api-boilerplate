e57d22309818a66f2fecb82db04675b1e5518fa9
solenovex <solenovex@live.com>
2017-11-25 20:59:44 +0800

    update

--- SalesApi.ViewModels/Settings/SubAreaEditViewModel.cs
+++ SalesApi.ViewModels/Settings/SubAreaEditViewModel.cs
@@ -1,9 +1,12 @@
-﻿namespace SalesApi.ViewModels.Settings
+﻿using Infrastructure.Features.Common;
+
+namespace SalesApi.ViewModels.Settings
 {
-    public class SubAreaEditViewModel
+    public class SubAreaEditViewModel: IDeleted
     {
         public int DeliveryVehicleId { get; set; }
         public string LegacySubAreaId { get; set; }
         public string Name { get; set; }
+        public bool Deleted { get; set; }
     }
 }
